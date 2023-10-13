2d782a0b9fd843d898f6011f55504498442326b4
Jose Serrano <josejasm.15@gmail.com>
2023-10-13 14:23:31 -0400

    fc

--- hack_10.py
+++ hack_10.py
@@ -5,4 +5,12 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 def fn_hack_10():
     result = "fooziman"
     #...
+
+    result = result.upper()
+
+    result = result.replace("O","0").replace("I","1").replace("A","@")
+
+    result = result[0].split()+result[1].split()+result[2].split()+result[3].split()+result[4].split()+result[5].split()+result[6].split()+result[7].split()
+
+
     return result  
--- hack_4.py
+++ hack_4.py
@@ -5,4 +5,7 @@ text: "fooziman" output => "foozimaN"
 def fn_hack_4():
     result = "fooziman"
     #...
+
+    result = result[0:7] + result[-1].upper()
+
     return result
